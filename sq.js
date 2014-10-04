d3e09f5c7b028ce4017ac8b4e97a3eb6032d6aa9
lindamariemummy <lindamariemummy@gmail.com>
2014-10-03 20:51:44 -0700

    update visualization

--- sq.js
+++ sq.js
@@ -1,69 +1,96 @@
 $(function() {
 	var dataStructureType = "";
+	$("#dataEntry").hide();
 
 	//runs if the user wants to visualize a stack
 	$("#stackButton").on("click", function() {
 		dataStructureType = "stack";
-		$("#queueButton").fadeOut();
+		$("#queueButton").hide();
+		$("#stackButton").hide();
+		$("#title").text("Visualization of a stack data structure");
+		$("#dataEntry").show();
 		stack();
 	});
 
 	//runs if the user wants to visualize a queue
 	$("#queueButton").on("click", function() {
 		dataStructureType = "queue";
-		$("#stackButton").fadeOut();
+		$("#stackButton").hide();
+		$("#queueButton").hide();
+		$("#title").text("Visualization of a queue data structure");
+		$("#dataEntry").show();
 		queue();
 	});
 
 	function stack() {
 
-		$("#selection").html("<button>push (add)</button><button>pop (remove)</button>");
+		$("#selection").html("<button id='push'>push (add)</button><button id='pop'>pop (remove)</button>");
 
-		$("#dataEntry").on("blur", function(){
-			//userInput = this.text();
-
+		//listens for a click on the push button
+		
+		$("#push").on("click", function(){			
+			//reads in entry from textbox
+			$dataEntry = $("#textBox").val();
+			if ($dataEntry === "") {
+				$("#error").html("<em>Don't you want to type something?</em>");
+			}
+			else {
+				$("#error").html("");			
+				pushStack($dataEntry);
+				$("#textBox").val("");
+			}
 		});
-		console.log("hi");
-		$(".node")
+
+		$("#pop").on("click", function(){
+			popStack();			
+		});
 	}
 
 	function queue() {
 		
+		//adds enqueue and unqueue buttons
+		$("#selection").html("<button id='enqueue'>enqueue (add)</button><button id='unqueue'>unqueue (remove)</button>");
+
 		//listens for a click on the enqueue button
 		$("#enqueue").on("click", function(){
 			console.log("hello");
-
-			//adds enqueue and unqueue buttons
-			$("#dataEntry").append("<button id='enqueue'>enqueue (add)</button><button id='unqueue'>unqueue (remove)</button>");
 			
 			//reads in entry from textbox
 			$dataEntry = $("#textBox").val();
 			if ($dataEntry === "") {
-				$("#dataEntry").append("Don't you want to type something?");
+				$("#error").html("<em>Don't you want to type something?</em>");
 			}
 			else {
+				$("#error").html("");			
 				enqueue($dataEntry);
-				$("#dataEntry").text("");
-				console.log("hi");
+				$("#textBox").val("");
 			}
-	});
+		});
+
+		$("#unqueue").on("click", function(){
+			unqueue();			
+		});
 }
 
-	function push(text) {
-		$("ul").append("<li id='dataValue'>"+text+"</li>");
+	function pushStack(text) {
+		$("ul").prepend("<li id='dataValue'>"+text+"</li>");
 	}
 
-	function pop() {
-		$("ul").lastChild.remove();
+	function popStack(text) {
+		//$("ul:first-child").remove();
+		$removeItem = $("li").filter(":first-child");	
+		$removeItem.slideUp("normal", function(){$(this).remove();});
 	}
 
-	function enqueue() {
+	function enqueue(text) {
 		$("ul").prepend("<li id='dataValue'>"+text+"</li>");
 	}
 
 	function unqueue() {
-		("ul").firstChild.remove();
+		$removeItem = $("li").filter(":last-child");	
+		$removeItem.slideUp("normal", function(){$(this).remove();});
 	}
+
 });
 
 
